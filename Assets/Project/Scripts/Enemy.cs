993813bf0ff70df73452501cce967e030c11d673
장우형 <janghy95@naver.com>
2025-07-04 16:32:01 +0900

    player, enemy 공격

--- Assets/Project/Scripts/Enemy.cs
+++ Assets/Project/Scripts/Enemy.cs
@@ -1,16 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
+using UnityEngine.SocialPlatforms;
 
 namespace TowerDefense
 {
     public class Enemy : Character
     {
-        public float moveSpeed;
+        Animator animator;
 
+        public float moveSpeed;
+        public float rayDistance;
+        public string charName;
+        private bool isAttack = false;
+
+        void Awake()
+        {
+            animator = GetComponent<Animator>();
+        }
+        protected override void Start()
+        {
+            base.Start();
+        }
         void Update()
         {
-            //TODO: raycasthit 쓰기
+            Vector2 rayStartpos = new Vector2(transform.position.x, transform.position.y + 1f);
+            //TODO: raycasthit 수정 완료하기
+            RaycastHit2D rayHit = Physics2D.Raycast(rayStartpos, Vector2.left, rayDistance);
+            Debug.DrawRay(rayStartpos, Vector2.left * rayDistance, new Color(1, 0, 0, 1));
+
+            Move();
+
+            if (Time.time > lastAttackTime + attackInterval)
+            {
+                moveSpeed = 0f;
+                Attack(rayHit);
+                lastAttackTime = Time.time;
+            }
+
+            Die();
+            //TODO: 프리스트는 소환되자마자 animator.SetBool("isSpell", isSpell); 애니메이션 동작, GameManager에서 소환 담당?
+        }
+        public void Move()
+        {
+            transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+            animator.SetInteger("State", 2);
+        }
+        void Attack(RaycastHit2D ray)
+        {
+            if (ray.collider != null)
+            {
+                //Debug.Log("Enemy Ray Hit: " + ray.collider.name);
+
+                if (ray.collider.CompareTag("Player"))
+                {
+                    //Debug.Log("Enemy Hit Player!");
+
+                    Playerable playerInfo = ray.collider.GetComponent<Playerable>();
+                    playerInfo.TakeDamage(damage);
+
+                    isAttack = true;
+                    animator.SetBool("Attack", isAttack);
+                }
+            }
+        }
+        void Die()
+        {
+            if (currentHp <= 0)
+            {
+                animator.SetInteger("State", 9);
+                //TODO: 오브젝트풀에서 회수
+            }
         }
 
     }
--- Assets/Project/Scripts/Playerable.cs
+++ Assets/Project/Scripts/Playerable.cs
@@ -19,17 +19,25 @@ namespace TowerDefense
         {
             animator = GetComponent<Animator>();
         }
+        protected override void Start()
+        {
+            base.Start();
+        }
 
         void Update()
         {
+            Vector2 rayStartpos = new Vector2(transform.position.x, transform.position.y + 0.2f);
             //TODO: raycasthit 수정 완료하기
-            RaycastHit2D rayHit = Physics2D.Raycast(transform.position, Vector2.right, rayDistance);
-            Debug.DrawRay(transform.position, Vector2.right * rayDistance, new Color(0, 1, 0, 1), rayDistance);
+            RaycastHit2D rayHit = Physics2D.Raycast(rayStartpos, Vector2.right, rayDistance);
+            Debug.DrawRay(rayStartpos, Vector2.right * rayDistance, new Color(0, 1, 0, 1), rayDistance);
 
             if (Time.time > lastAttackTime + attackInterval)
             {
-                Attack(rayHit);
-                lastAttackTime = Time.time;
+                if(charName != "Priest")
+                {
+                    Attack(rayHit);
+                    lastAttackTime = Time.time;
+                }
             }
 
             Die();
@@ -37,11 +45,16 @@ namespace TowerDefense
         }
         void Attack(RaycastHit2D ray)
         {
+            //Debug.Log(">> Player 공격 시도");
+
             if (ray.collider != null && ray.collider.CompareTag("Enemy"))
             {
-                Character enemyInfo = ray.collider.GetComponent<Character>();
+                //Debug.Log("Player Ray Hit: " + ray.collider.name);
+
+                Enemy enemyInfo = ray.collider.GetComponent<Enemy>();
                 isAttack = true;
                 animator.SetBool("isAttack", isAttack);
+                //Debug.Log(">> Enemy에게 데미지 줌!");
                 enemyInfo.TakeDamage(damage);
             }
             else
